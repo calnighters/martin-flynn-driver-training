e59c05c419e89396f2cd17f3472ae91c3f6ebd0a
Callum Nightingale <callum.nightingale@capgemini.com>
2022-04-25 18:33:49 +0100

    refactor: extract constants to env variables

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,13 @@
 import { WhatsApp } from "@mui/icons-material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
-const facebookUrl =
-  "https://www.facebook.com/Martin-Flynn-Driving-106274308474591/";
-const phoneNumber = "07772230455";
-const email = "tango7864@gmail.com";
-const highwayCode =
-  "https://www.gov.uk/browse/driving/highway-code-road-safety";
-const helpfulInfo = "https://www.gov.uk/browse/driving/learning-to-drive";
-const theoryPrice = 25;
-const practicalPrice = 62;
+const facebookUrl = process.env.REACT_APP_FACEBOOK_URL;
+const phoneNumber = process.env.REACT_APP_PHONE_NUMBER;
+const email = process.env.REACT_APP_EMAIL;
+const highwayCode = process.env.REACT_APP_HIGHWAY_CODE;
+const helpfulInfo = process.env.REACT_APP_HELPFUL_INFO;
+const theoryPrice = process.env.REACT_APP_THEORY_PRICE;
+const practicalPrice = process.env.REACT_APP_PRACTICAL_PRICE;
 
 const externalUrls = [
   {
